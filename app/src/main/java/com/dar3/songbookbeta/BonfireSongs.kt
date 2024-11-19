6a8bb16461bb2f261a47ffe334cce91475897886
Darko <darkoshypka@gmail.com>
2024-11-20 00:00:34 +0100

    Added screen changing for main buttons

--- app/src/main/java/com/dar3/songbookbeta/BonfireSongs.kt
+++ app/src/main/java/com/dar3/songbookbeta/BonfireSongs.kt
@@ -1,10 +1,10 @@
 package com.dar3.songbookbeta
 
 import android.annotation.SuppressLint
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -29,31 +29,22 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.dar3.songbookbeta.ui.theme.SongbookBetaTheme
 
-data class BottomNavigationItem(
-    val title: String,
-    val selectedIcon: ImageVector,
-    val unselectedIcon: ImageVector,
-    val hasNews: Boolean,
-    val badgeCount: Int? = null
-
-)
-
-
-class MainActivity : ComponentActivity() {
+class BonfireSongs : ComponentActivity() {
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             SongbookBetaTheme {
                 val items = listOf(
@@ -106,7 +97,7 @@ class MainActivity : ComponentActivity() {
                                         selected = selectedItemIndex == index,
                                         onClick = {
                                             selectedItemIndex = index
-                                            // navController.navigate(item.title)
+//                                             navController.navigate(item.title)
                                         },
                                         label = {
                                             Text(text = item.title)
@@ -139,28 +130,11 @@ class MainActivity : ComponentActivity() {
                     ) {
 
 
-                        LastSongsButton { buttonText ->
-                            when (buttonText) {
-                                "LastPlayed" -> {
-                                    val intent = Intent(this@MainActivity, LastSongs::class.java)
-                                    startActivity(intent)
-                                }
-                                "AllSongs" -> {
-                                    val intent = Intent(this@MainActivity, AllSongs::class.java)
-                                    startActivity(intent)
-                                }
-                                "FavouriteSongs" -> {
-                                    val intent = Intent(this@MainActivity, FavouriteSongs::class.java)
-                                    startActivity(intent)
-
-                                } "BonfireSongs" -> {
-                                val intent = Intent(this@MainActivity, BonfireSongs::class.java)
-                                startActivity(intent)
-                                }
-                            }
-                        }
-
-
+                        MainTextBonfireSongs(
+                            message = "Bonfire songs",
+                            modifier = Modifier
+                                .padding(5.dp)
+                        )
 
 
                     }
@@ -171,53 +145,20 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-
 @Composable
-fun LastSongsButton(onClick: (String) -> Unit) {
+fun MainTextBonfireSongs(message: String, modifier: Modifier = Modifier) {
     Column(
-    modifier = Modifier
-        .fillMaxSize()
-        .padding(16.dp),
-    verticalArrangement = Arrangement.spacedBy(8.dp)
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
     ) {
-        TextButton(
-            onClick = { onClick("LastPlayed") }
-        ) {
-            Text(text = "Last played songs",
-                modifier = Modifier
 
-            )
-        }
-
-        TextButton(
-            onClick = { onClick("AllSongs") }
-        ) {
-            Text(text = "All songs",
-                modifier = Modifier
-
-            )
-        }
-
-        TextButton(
-            onClick = { onClick("FavouriteSongs") }
-        ) {
-            Text(text = "Favourite songs",
-                modifier = Modifier
-
-            )
-        }
-
-        TextButton(
-            onClick = { onClick("BonfireSongs") }
-        ) {
-            Text(text = "Bonfire songs",
-                modifier = Modifier
-
-            )
-        }
+        Text(
+            text = message,
+            fontSize = 20.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+        )
     }
+
 }
-
-
-
 
