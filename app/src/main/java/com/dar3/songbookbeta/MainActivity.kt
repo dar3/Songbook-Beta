86cd1784e450f1e6d85abfde68a1866b8c0bbe0c
Darko <darkoshypka@gmail.com>
2024-12-04 17:22:10 +0100

    Added songs on AllSongsScreen

--- app/src/main/java/com/dar3/songbookbeta/MainActivity.kt
+++ app/src/main/java/com/dar3/songbookbeta/MainActivity.kt
@@ -7,8 +7,10 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
@@ -22,6 +24,7 @@ import androidx.compose.material.icons.outlined.AddCircle
 import androidx.compose.material.icons.outlined.Face
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
@@ -31,8 +34,10 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -232,25 +237,77 @@ fun HomeScreen(navController: NavController) {
 
 @Composable
 fun AllSongsScreen() {
-    Text(
-        text = "It's my life",
-        fontSize = 20.sp,
+    Column(
         modifier = Modifier
+            .fillMaxSize()
             .padding(16.dp)
+    ) {
+        Text(
+            text = "It's my life",
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp,
+            modifier = Modifier
+                .padding(bottom = 8.dp)
 
-    )
+        )
 
-    Text(
-        text = "Bon Jovi",
-        fontSize = 15.sp,
-        lineHeight = 80.sp,
-        modifier = Modifier
-            .padding(16.dp)
+        Text(
+            text = "Bon Jovi",
+            fontSize = 15.sp,
+            lineHeight = 20.sp,
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
 
-    )
+        Text(
+            text = "This ain't a song for the broken-hearted\n" +
+                    "No silent prayer for the faith-departed...",
+            fontSize = 15.sp,
+            lineHeight = 20.sp,
+            modifier = Modifier.fillMaxWidth()
+        )
 
+        HorizontalDivider(
+            modifier = Modifier.fillMaxWidth(),
+            thickness = 1.dp,
+            color = Color.Gray,
 
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+            // next song below
+
+        Text(
+            text = "Старі фотографії",
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp,
+            modifier = Modifier
+                .padding(bottom = 8.dp)
+
+        )
+
+        Text(
+            text = "Скрябін",
+            fontSize = 15.sp,
+            lineHeight = 20.sp,
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+
+        Text(
+            text = "Здається, шо то було так давно,\n" +
+                    "Коли в руках тримаю цей альбом...",
+            fontSize = 15.sp,
+            lineHeight = 20.sp,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        HorizontalDivider(
+            modifier = Modifier.fillMaxWidth(),
+            thickness = 1.dp,
+            color = Color.Gray
+        )
+    }
 }
+
 
 @Composable
 fun BonfireSongsScreen() {
