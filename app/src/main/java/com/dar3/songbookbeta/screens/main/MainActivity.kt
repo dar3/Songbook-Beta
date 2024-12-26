4a9274ea5950c26fe7d6236b7becc3e7049bd79b
Darko <darkoshypka@gmail.com>
2024-12-26 16:52:11 +0100

    Added TopBar

--- app/src/main/java/com/dar3/songbookbeta/screens/main/MainActivity.kt
+++ app/src/main/java/com/dar3/songbookbeta/screens/main/MainActivity.kt
@@ -10,13 +10,26 @@ import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.core.animation.doOnEnd
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.dar3.songbookbeta.R
 import com.dar3.songbookbeta.screens.AddSongScreen
 import com.dar3.songbookbeta.screens.FavouriteSongsScreen
 import com.dar3.songbookbeta.screens.LastSongsScreen
@@ -31,9 +44,9 @@ class MainActivity : ComponentActivity() {
 
     private val viewModel by viewModels<MainViewModel>()
 
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
 
         // region Splash screen
         installSplashScreen().apply {
@@ -68,14 +81,44 @@ class MainActivity : ComponentActivity() {
                 zoomY.start()
             }
         }
+        enableEdgeToEdge()
+
         // endregion
 
         // region Main content
         setContent {
             SongbookBetaTheme {
                 val navController = rememberNavController()
-
+                val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior(rememberTopAppBarState())
                 Scaffold(
+                    modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+                    topBar = {
+                        MediumTopAppBar(
+                            colors = TopAppBarDefaults.topAppBarColors(
+                                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                                titleContentColor = MaterialTheme.colorScheme.primary,
+                            ),
+                            title = {
+                                Text(
+                                    getString(R.string.app_name),
+                                    maxLines = 1,
+                                    overflow = TextOverflow.Ellipsis
+                                )
+                            },
+                            navigationIcon = {
+                                IconButton(onClick = {  navController.navigateUp()}) {
+                                    Icon(
+                                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                                        contentDescription = "Localized description"
+                                    )
+                                }
+                            },
+
+                            scrollBehavior = scrollBehavior
+                        )
+                    },
+
+
                     bottomBar = {
                         BottomNavigationBar(navController)
                     },
